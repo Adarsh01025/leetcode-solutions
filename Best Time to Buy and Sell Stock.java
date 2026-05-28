2e0c1db300cbe7e65f07d3e47082e8f0a85c2b2e
Adarsh01025 <135800626+Adarsh01025@users.noreply.github.com>
2026-05-28 22:38:08 +0530

    Add files via upload

---
+++ Best Time to Buy and Sell Stock.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buy = prices[0];
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < buy) {
+                buy = prices[i];
+            } else if (prices[i] - buy > profit) {
+                profit = prices[i] - buy;
+            }
+        }
+        return profit;
+    }
+}
+
+
+
+
+//failed due to time complexity
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//         int max = 0;
+//         for(int i =0; i<prices.length; i++){
+//             for (int j = i+1; j<prices.length; j++){
+//                 if(prices[j] - prices[i] > max){
+//                     max = prices[j] - prices[i];
+//                 }
+//             }
+//         }
+//         return max;
+//     }
+// }
