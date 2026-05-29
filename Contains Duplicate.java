ee5c01993f9e2809d29057c3a32b910ceaeb2906
Adarsh01025 <135800626+Adarsh01025@users.noreply.github.com>
2026-05-29 23:02:17 +0530

    Add files via upload

---
+++ Contains Duplicate.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        // int n = nums.length;
+        // for (int i = 0; i < n - 1; i++) {
+        //     for (int j = i + 1; j < n; j++) {
+        //         if (nums[i] == nums[j])
+        //             return true;
+        //     }
+        // }
+        // return false;
+
+
+        Arrays.sort(nums);
+        int n = nums.length;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1])
+                return true;
+        }
+        return false;
+    }
+}
