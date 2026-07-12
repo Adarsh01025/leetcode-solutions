9d2c197565e2d63d7d872c080b997a7bef8aaa3e
Adarsh01025 <135800626+Adarsh01025@users.noreply.github.com>
2026-07-12 19:27:53 +0530

    Add files via upload

---
+++ Game Play Analysis IV.sql
@@ -0,0 +1,18 @@
+-- Write your PostgreSQL query statement below
+WITH first_login AS (
+    SELECT
+        player_id,
+        MIN(event_date) AS first_date
+    FROM Activity
+    GROUP BY player_id
+)
+SELECT
+    ROUND(
+        COUNT(DISTINCT f.player_id)::NUMERIC /
+        (SELECT COUNT(DISTINCT player_id) FROM Activity),
+        2
+    ) AS fraction
+FROM first_login f
+JOIN Activity a
+    ON f.player_id = a.player_id
+   AND a.event_date = f.first_date + INTERVAL '1 day';
